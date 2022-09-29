2473caf82d78c06bd1edb4586fa97756445864e7
Pabel Hernández <pabel.hernandez@live.com>
2022-09-29 05:20:56 -0600

    E1
    exercises 1 and 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello my friend, What's your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
+
---
+++ Exercise1-2/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!, the current time is...");
+DateTime aDate = DateTime.Now;
+Console.WriteLine(aDate.ToString("H:mm"));
