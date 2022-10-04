ed73eacd2f5654b5537e2334febf7d1c996d48ab
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-04 10:18:47 -0600

    E2
    update more details

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,11 +11,11 @@ int age = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Write an username:");
 string userName = Console.ReadLine();
 
-Console.WriteLine("finally... does " + userName + " know how to program?");
-string developer = Console.ReadLine();
+Console.WriteLine("finally... does " + userName + " know how to program? (T/F)");
+bool developer = Convert.ToBoolean(Console.ReadLine());;
 
 Console.WriteLine("");
 Console.WriteLine("Welcome " + name + " " + lastName + "!!! " + "your Username is: " + userName + " and you are " + age + " years old");
 
-Console.WriteLine("Welcome " + name + " " + lastName + "!!!" + "your Username is: " + userName + "and you are " + age + " years old");
+Console.WriteLine("and the affirmation..." + userName + " is a developer is: " + developer);
 
