3072f5c485ca542c9c95dc4679544cc22dd6bc23
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-04 10:51:43 -0600

    E2.1
    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -26,3 +26,29 @@ Console.WriteLine("Welcome " + name + " " + lastName + "!!! " + "your Username i
 
 Console.WriteLine("and the affirmation..." + userName + " is a developer is: " + developer);
 
+// Exercise 2.2
+Console.WriteLine("Tell me... How is the car of your dreams?");
+    Console.WriteLine("");
+    
+    Console.WriteLine("what is the brand of the car?...");
+    string brand = Console.ReadLine();
+    
+    Console.WriteLine("what is the model?");
+    string model = Console.ReadLine();
+    
+    Console.WriteLine("how many doors?");
+    int doors = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine("What engine does it have?");
+    double engine = Convert.ToDouble(Console.ReadLine());
+    
+    Console.WriteLine("is it all terrain?(T/F)");
+    bool allT = Convert.ToBoolean(Console.ReadLine());
+    
+    Console.WriteLine("");
+    
+    Console.WriteLine(brand);
+    Console.WriteLine(model);
+    Console.WriteLine(doors);
+    Console.WriteLine(engine);
+    Console.WriteLine(allT);
