7d95be9b17797bf934cd2b951b2a20feb79cb3b1
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-05 13:34:08 -0600

    update
    update variables

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,33 +1,28 @@
-﻿// Exercise 2.2
-Console.WriteLine("Tell me... How is the car of your dreams?");
-Console.WriteLine("");
+﻿Console.WriteLine("Tell me... How is the car of your dreams?");
 
 Console.WriteLine("what is the brand of the car?...");
 string brand = Console.ReadLine();
 
-Console.WriteLine("what is the model?");
+Console.WriteLine("What is the model?");
 string model = Console.ReadLine();
 
-Console.WriteLine("how many doors?");
+Console.WriteLine("How many doors?");
 int doors = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("What engine does it have?");
 double engine = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("is it all terrain?(T/F)");
+Console.WriteLine("Is it all terrain?(T/F)");
 bool allT = Convert.ToBoolean(Console.ReadLine());
 
-Console.WriteLine("what is the class?");
+Console.WriteLine("What is the class?");
 char ch1 = Convert.ToChar(Console.ReadLine());
 
 Console.WriteLine("");
 
-
-Console.WriteLine("Brand: " + brand );
-Console.WriteLine(model);
-Console.WriteLine(doors);
-Console.WriteLine(engine);
-Console.WriteLine(allT);
-Console.WriteLine(ch1);
-
-Console.WriteLine("");
+Console.WriteLine("Brand: " + brand + " (The data type is: " + brand.GetType() + ").");
+Console.WriteLine("Model: " + model + " (The data type is: " + model.GetType() + ").");
+Console.WriteLine("Doors: " + doors + " (The data type is: " + doors.GetType() + ").");
+Console.WriteLine("Engine: " + engine + " (The data type is: " + engine.GetType() + ").");
+Console.WriteLine("All Terrain: " + allT + " (The data type is: " + allT.GetType() + ").");
+Console.WriteLine("Class: " + ch1 + " (The data type is: " + ch1.GetType() + ").");
