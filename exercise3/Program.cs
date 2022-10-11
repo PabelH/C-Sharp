fc4e9c66d6bba87536601caa13171ca9ceeca4da
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-11 08:42:03 -0600

    ph
    phData

--- exercise3/Program.cs
+++ exercise3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Data myData = new Data("Pabel Hernandez", 6143552795, "Vista Sierra Azul", "pabel_h@hotmail.com", true);
+
+Console.Writeline(myData.fullName);
+
+
+
+
+
+
+public struct Data
+{
+
+    public Data(string n, int ph, string ad, string em, bool nC)
+    {
+      fullName = n;
+      phone = ph;
+      address = ad;
+      email = em;
+      newClient = nC;
+    }
+
+    public string fullName { get; set; }
+    public int phone { get; set; }
+    public string address { get; set; }
+    public string email { get; set; }
+    public bool newClient { get; set; }
+
+    public override string ToString() => $"({phone}, {newClient})";
+
+}
