83b9827cfcc351ef117f9b9beef41e7fb0777a45
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-13 10:39:34 -0600

    E4
    e4-2

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -1,4 +1,5 @@
 ﻿//While Loop
+
 int i = 0;
 int numU = 0;
 Console.WriteLine("Give me a number MF!!!... (My Friend)");
@@ -8,4 +9,28 @@ while (i<=20)
 {
     Console.WriteLine($"{i} X {numU} = {numU * i} ");
     i++;
+}
+
+//Do..While Loop
+int j = 0;
+//int numU1 = 0;
+Console.WriteLine("Give me a number MF!!!... (My Friend)");
+j = Convert.ToInt32(Console.ReadLine());
+
+if(j > 0){
+do
+{
+    Console.WriteLine(j);
+    j--;
+}while (j > 0);
+} else if (j < 0) 
+{
+    do
+    {
+        Console.WriteLine(j);
+        j++;
+    }while (j < 0);
+} else
+{
+  Console.WriteLine("Try with other number...");
 }
