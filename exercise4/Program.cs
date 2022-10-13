fb0d4fa5943501c0db95b1c7ffee1ba66427242e
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-13 11:20:08 -0600

    e4 u
    E4 update

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -34,3 +34,21 @@ do
 {
   Console.WriteLine("Try with other number...");
 }
+
+//foor loop
+int width = 0, height = 0;
+
+width = int.Parse(Console.ReadLine());
+height = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= height; i++)
+{
+    for (int j = 1; j <= width; j++)
+    {
+        if ((i == 1 || i == height || j == 1 || j == width))
+            Console.Write("*");
+        else
+            Console.Write(" ");
+    }
+    Console.WriteLine();
+}
