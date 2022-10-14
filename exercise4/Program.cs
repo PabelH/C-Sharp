686b878dfc259703972f49411adb74b7a64ce564
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-14 08:48:56 -0600

    e4
    ex4 part 3

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -36,12 +36,21 @@ do
 }
 
 //foor loop
-int width = 0, height = 0;
+int width = 0;
+int height = 0;
+bool fill = true;
 
-width = int.Parse(Console.ReadLine());
-height = int.Parse(Console.ReadLine());
+Console.WriteLine("Give me the widht number");
+width = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i <= height; i++)
+Console.WriteLine("Give me the height number");
+height = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("do you want to fill it?");
+fill = Convert.ToBoolean(Console.ReadLine());
+
+if (fill == false) {
+    for (int i = 1; i <= height; i++)
 {
     for (int j = 1; j <= width; j++)
     {
@@ -51,4 +60,17 @@ for (int i = 1; i <= height; i++)
             Console.Write(" ");
     }
     Console.WriteLine();
+}
+}else
+{
+    for (int i = 0; i < height; i++) 
+    {
+        for (int j = 0; j < width; j++) 
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+    }
+
 }
