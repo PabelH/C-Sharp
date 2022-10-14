55957ea57e07273fb9c0151ae4551516205df7eb
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-14 09:11:49 -0600

    e4
    E4 finished

--- exercise4/Program.cs
+++ exercise4/Program.cs
@@ -39,7 +39,7 @@ do
 int width = 0;
 int height = 0;
 bool fill = true;
-
+int quant = 0;
 Console.WriteLine("Give me the widht number");
 width = Convert.ToInt32(Console.ReadLine());
 
@@ -49,28 +49,36 @@ height = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("do you want to fill it?");
 fill = Convert.ToBoolean(Console.ReadLine());
 
-if (fill == false) {
-    for (int i = 1; i <= height; i++)
+Console.WriteLine("hoy many do you want handsome?");
+quant = Convert.ToInt32(Console.ReadLine());
+
+for (int k = 0; k < quant; k++)
 {
-    for (int j = 1; j <= width; j++)
-    {
-        if ((i == 1 || i == height || j == 1 || j == width))
-            Console.Write("*");
-        else
-            Console.Write(" ");
-    }
-    Console.WriteLine();
-}
-}else
-{
-    for (int i = 0; i < height; i++) 
-    {
-        for (int j = 0; j < width; j++) 
+
+    if (fill == false) {
+        for (int i = 1; i <= height; i++)
         {
-            Console.Write("*");
+            for (int j = 1; j <= width; j++)
+            {
+                if ((i == 1 || i == height || j == 1 || j == width))
+                    Console.Write("*");
+                else
+                    Console.Write(" ");
+            }
+        Console.WriteLine();
+        }
+    }else
+    {
+        for (int i = 0; i < height; i++) 
+        {
+            for (int j = 0; j < width; j++) 
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
         }
 
-        Console.WriteLine();
     }
-
+    Console.WriteLine();
 }
