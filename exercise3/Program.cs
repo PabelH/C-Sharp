b3b6de069471e80a80a3ac1bb35010c8f8249610
Pabel Hernandez <pabel.hernandez@live.com>
2022-10-17 07:32:17 -0600

    E5
    finishing E5

--- exercise3/Program.cs
+++ exercise3/Program.cs
@@ -63,21 +63,36 @@ public class HelloWorld
         languages.Add("Java");
         languages.Add("C#");
         
-        Console.WriteLine("Welcome to the programming games");
-        Console.WriteLine("presently we have these languages");
+        Console.WriteLine("Welcome to the programming games...");
+        Console.WriteLine("presently we have these languages:");
         foreach (string a in languages){
         Console.WriteLine(ind + " " + a);
         ind ++;
         }
-        Console.WriteLine("select your favorite programming language");
-        //save user answer 
-        //create conditional switch
+        Console.WriteLine("Write the NUMBER of your favorite programming language");
+        ind = Convert.ToInt32(Console.ReadLine());
+        
+        switch (ind) 
+        {
+          case 1:
+            Console.WriteLine("Nice selection... wait for instructions");
+            break;
+          case 2:
+            Console.WriteLine("Nice selection... wait for instructions");
+            break;
+          case 3:
+            Console.WriteLine("Nice selection... wait for instructions");
+            break;
+          case 4:
+            Console.WriteLine("Nice selection... wait for instructions");
+            break;
+          default:
+             Console.WriteLine($"No valid language... you lose before start =(");
+            break;
+        }
         
         
     }
 }
-
-
-
 */
 
