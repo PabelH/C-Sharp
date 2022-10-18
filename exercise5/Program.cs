7e7034760de53d018da5d32333e2f92c0be81788
Pabel Hernández <pabel.hernandez@live.com>
2022-10-18 04:25:21 -0600

    e5
    finish

--- exercise5/Program.cs
+++ exercise5/Program.cs
@@ -1,8 +1,9 @@
-﻿E5 part one
+﻿
 string name = "";
 string email = "";
 string promo = "PROMO1234";
 int price = 100;
+
 Console.WriteLine("Hello Friend, what's your name?");
 name = Console.ReadLine();
 Console.WriteLine($"Welcome {name}!!!, now tell me your email");
@@ -16,36 +17,38 @@ if (promo == "PROMO1234"){
     Console.WriteLine($"Sorry no valid promo the price is: {price}");
 }
 
-//E5 part two
 
-
-using System.Collections.Generic;
-
-
-
-    {
-        int select = 0;
-        int ind = 1;
-        List<string> languages = new List<string>();
-        languages.Add("Java Script");
-        languages.Add("Python");
-        languages.Add("Java");
-        languages.Add("C#");
+/*
+int select = 0;
+int ind = 1;
+var languages = new List<string> {"Java Script", "Python", "Java", "C#"};
         
-        Console.WriteLine("Welcome to the programming games");
-        Console.WriteLine("presently we have these languages");
-        foreach (string a in languages){
-        Console.WriteLine(ind + " " + a);
-        ind ++;
-        }
-        Console.WriteLine("select your favorite programming language");
-        //save user answer 
-        //create conditional switch
+Console.WriteLine("Welcome to the programming games...");
+Console.WriteLine("presently we have these languages:");
+foreach (string a in languages){
+Console.WriteLine(ind + " " + a);
+ind ++;
+}
+Console.WriteLine("Write the NUMBER of your favorite programming language");
+ind = Convert.ToInt32(Console.ReadLine());
         
-        
-    }
-
-
-
-
+switch (ind) 
+{
+    case 1:
+    Console.WriteLine("Nice selection... wait for instructions");
+    break;
+    case 2:
+    Console.WriteLine("Nice selection... wait for instructions");
+    break;
+    case 3:
+    Console.WriteLine("Nice selection... wait for instructions");
+    break;
+    case 4:
+    Console.WriteLine("Nice selection... wait for instructions");
+    break;
+    default:
+        Console.WriteLine($"No valid language... you lose before start =(");
+    break;
+}     
+*/
 
